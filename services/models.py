0c1feca559570e81391c4f80f3aa2ee81d46d7b6
Andrei Makarov <aomak@yandex.ru>
2025-12-18 18:03:39 +0300

    добавлены тесты на модели и 200

--- services/models.py
+++ services/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 
-from users.models import CustomUser
 
 
 class MainPage(models.Model):
